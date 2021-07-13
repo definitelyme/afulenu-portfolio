7d5f62534a2abcfcc13350d97ccecdf230ff3a78
Brendan Ejike <ejike.br@gmail.com>
2021-07-13 11:55:22 +0100

    first commit

---
+++ web/init.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5WoJ_lkYFyKHFVmpn7VgZgU9jjvsuKbw",
+  authDomain: "brendan-portfolio.firebaseapp.com",
+  databaseURL: "https://brendan-portfolio.firebaseio.com",
+  projectId: "brendan-portfolio",
+  storageBucket: "brendan-portfolio.appspot.com",
+  messagingSenderId: "999975491629",
+  appId: "1:999975491629:web:6e3cf8d7e3cd28648c40a4",
+  measurementId: "G-RR56ZBL0S8",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
