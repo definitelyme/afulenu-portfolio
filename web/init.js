82cbbb15ff2393978f79cfc3a2cefd9c5d4d7cd3
Brendan Ejike <ejike.br@gmail.com>
2021-07-13 13:00:07 +0100

    Install firebase & setup analytics

--- web/init.js
+++ web/init.js
@@ -1,13 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5WoJ_lkYFyKHFVmpn7VgZgU9jjvsuKbw",
-  authDomain: "brendan-portfolio.firebaseapp.com",
-  databaseURL: "https://brendan-portfolio.firebaseio.com",
-  projectId: "brendan-portfolio",
-  storageBucket: "brendan-portfolio.appspot.com",
-  messagingSenderId: "999975491629",
-  appId: "1:999975491629:web:6e3cf8d7e3cd28648c40a4",
-  measurementId: "G-RR56ZBL0S8",
+  apiKey: "AIzaSyB-oLdrp4TMjwVGzPaQ4fLle74ijOTS9xk",
+  authDomain: "x-afulenu.firebaseapp.com",
+  projectId: "x-afulenu",
+  storageBucket: "x-afulenu.appspot.com",
+  messagingSenderId: "429765954483",
+  appId: "1:429765954483:web:38401d633c2f0e5fde59c6",
+  measurementId: "G-TRMTFH6SQY",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
